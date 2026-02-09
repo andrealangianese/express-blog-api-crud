99ad14ef2e18e82b196777a53bb9178c3a06449b
andrealangianese <andre.langia.01@gmail.com>
2026-02-09 15:23:24 +0100

    copio le rotte crud in postcontrollers e le vado a rendere funzioni da esportare

---
+++ controllers/postControllers.js
@@ -0,0 +1,44 @@
+//creo  rotte crud
+
+// index
+function index(req, res) {
+    res.json(valoriPosts)
+};
+
+// show
+router.get('/:id', function (req, res) {
+
+    //var per salvare id
+
+    const idPosts = parseInt(req.params.id)
+
+    //confronto il valore di id
+
+    const post = valoriPosts.find(p => p.id === idPosts)
+    
+    res.json(post);
+});
+
+// store
+router.post('/', function (req, res) {
+    res.send('Creazione nuovi posts');
+});
+
+// update
+router.put('/:id', function (req, res) {
+    res.send('Modifica integrale dei posts ' + req.params.id);
+});
+
+// modify
+router.patch('/:id', function (req, res) {
+    res.send('Modifica parziale dei posts ' + req.params.id);
+});
+
+// destroy
+router.delete('/:id', (req, res) => {
+    res.send('Eliminazione dei posts ' + req.params.id);
+});
+
+//rendo importabili da altri componenti queste rotte 
+
+module.exports = router
