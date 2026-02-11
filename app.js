5b05529516f44685c7acdcd680f7c2864a82bede
andrealangianese <andre.langia.01@gmail.com>
2026-02-11 14:36:44 +0100

    registro middleware in app.js  dopo le rotte

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const port = 3000
 
 //importo il router dei post salvandolo in una var
 
-const routerPosts = require('./routers/posts')
+const routerPosts = require('./routers/posts');
+const errorsHandler = require("./middlewares/errorsHandler");
 
 
 // attivazione della cartella public per uso file statici
@@ -24,6 +25,10 @@ app.get("/", (req, res) => {
 //istanza per rendere utilizzabili le rotte per posts
 
 app.use('/posts' ,routerPosts)
+
+//registro middleware dopo le rotte l'errore 500
+
+app.use(errorsHandler)
 
 
 
